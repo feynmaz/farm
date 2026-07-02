574b1365b45df0a6346010c2e80a34eab8aaf222
Nikolai Mazein <feynmaz@gmail.com>
2026-07-03 01:30:59 +0500

    add server

---
+++ internal/server/middleware/monitoring.go
@@ -0,0 +1,84 @@
+package middleware
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/feynmaz/farm/internal/logger"
+)
+
+func NewMonitoringMiddleware(appVersion string, logger *logger.Logger) func(next http.Handler) http.Handler {
+	var (
+		version = prometheus.NewGauge(prometheus.GaugeOpts{
+			Name: "version",
+			Help: "Version information about this binary",
+			ConstLabels: map[string]string{
+				"version": appVersion,
+			},
+		})
+		httpRequestsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Count of all HTTP requests",
+		}, []string{"code", "method"})
+
+		httpRequestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+			Name: "http_request_duration_seconds",
+			Help: "Duration of all HTTP requests",
+		}, []string{"code", "handler", "method"})
+	)
+	version.Set(1)
+
+	prometheus.MustRegister(httpRequestsTotal)
+	prometheus.MustRegister(httpRequestDuration)
+	prometheus.MustRegister(version)
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if isExcludedFromMonitoring(r.URL.Path) {
+				next.ServeHTTP(w, r)
+				return
+			}
+			requestID := getRequestID(r.Context())
+			logger.Info().
+				Str("requestID", requestID).
+				Str("method", r.Method).
+				Str("path", r.URL.Path).
+				Msg("request started")
+
+			start := time.Now()
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+			next.ServeHTTP(ww, r)
+
+			duration := time.Since(start)
+			statusCode := strconv.Itoa(ww.Status())
+
+			httpRequestsTotal.WithLabelValues(statusCode, r.Method).Inc()
+			httpRequestDuration.WithLabelValues(
+				statusCode,
+				r.URL.Path,
+				r.Method,
+			).Observe(duration.Seconds())
+
+			logger.Info().
+				Str("requestID", requestID).
+				Str("method", r.Method).
+				Str("path", r.URL.Path).
+				Int("status", ww.Status()).
+				Dur("duration", duration).
+				Str("userAgent", r.UserAgent()).
+				Msg("request completed")
+		})
+	}
+}
+
+func isExcludedFromMonitoring(urlPath string) bool {
+	return (urlPath == "/metrics" ||
+		urlPath == "/healthcheck" ||
+		strings.Contains(urlPath, "swagger"))
+}
---
+++ internal/server/server.go
@@ -0,0 +1,88 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/pprof"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
+
+	"github.com/feynmaz/farm/internal/config"
+	"github.com/feynmaz/farm/internal/logger"
+	"github.com/feynmaz/farm/internal/server/middleware"
+)
+
+type Server struct {
+	cfg    *config.Config
+	logger *logger.Logger
+	srv    *http.Server
+	v1     http.Handler
+}
+
+func New(cfg *config.Config, logger *logger.Logger, v1 http.Handler) *Server {
+	return &Server{
+		cfg:    cfg,
+		logger: logger,
+		v1:     v1,
+	}
+}
+
+func (s *Server) Run(ctx context.Context) error {
+	s.srv = &http.Server{
+		Addr:         fmt.Sprintf(":%v", s.cfg.Server.Port),
+		BaseContext:  func(_ net.Listener) context.Context { return ctx },
+		Handler:      s.getRouter(),
+		ReadTimeout:  s.cfg.Server.ReadTimeout,
+		WriteTimeout: s.cfg.Server.WriteTimeout,
+	}
+
+	s.logger.Info().Msgf("server started on port %d", s.cfg.Server.Port)
+	return s.srv.ListenAndServe()
+}
+
+func (s *Server) getRouter() *chi.Mux {
+	router := chi.NewMux()
+
+	// Middleware
+	router.Use(middleware.RequestIDMiddleware)
+	router.Use(middleware.NewMonitoringMiddleware(s.cfg.App.Version, s.logger))
+
+	// Profiler
+	router.HandleFunc("/debug/pprof/", pprof.Index)
+	router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	router.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	router.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	router.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+	router.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+	router.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
+	router.Handle("/debug/pprof/block", pprof.Handler("block"))
+
+	// Metrics
+	router.Handle("/metrics", promhttp.Handler())
+
+	// Swagger
+	router.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL("/swagger/doc.json"),
+	))
+	router.Get("/swagger/doc.json", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, s.cfg.OpenapiPath)
+	})
+
+	// API
+	router.Mount("/api/v1", s.v1)
+
+	return router
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	s.logger.Info().Msg("graceful server shutdown")
+	err := s.srv.Shutdown(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to shutdown http server: %w", err)
+	}
+	return nil
+}
